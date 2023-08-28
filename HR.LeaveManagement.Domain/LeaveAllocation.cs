3f76efc96b82cfda050e094d075e9fdeb0159677
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-08-27 23:37:33 -0700

    Add project files.

---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+    public int NumberOfDays { get; set; }
+
+    public LeaveType? leaveType { get; set; }  // Why nullable? Can we have an allocation without a Leave Type? maybe...
+    public int LeaveTypeId { get; set; }
+    
+    public int Period { get; set; }
+}
