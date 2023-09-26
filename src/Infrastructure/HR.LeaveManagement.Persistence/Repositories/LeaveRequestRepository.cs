c04e2e0c5fb3bae0766560a72bf64e519cd4d600
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-09-25 20:32:56 -0700

    Leave Allocation and Request methods

--- src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.Repositories
 {
@@ -8,6 +9,25 @@ namespace HR.LeaveManagement.Persistence.Repositories
     {
         public LeaveRequestRepository(HrDatabaseContext context) : base(context)
         {
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            return await _context.LeaveRequests.Include(l => l.LeaveType)
+                                               .FirstOrDefaultAsync(l => l.Id == id);
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails()
+        {
+            return await _context.LeaveRequests.Include(l => l.LeaveType)
+                                               .ToListAsync();
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestWithDetails(string userId)
+        {
+            return await _context.LeaveRequests.Where(q => q.RequestingEmployeeId == userId)
+                                               .Include(l => l.LeaveType)                                         
+                                               .ToListAsync();
         }
     }
 
