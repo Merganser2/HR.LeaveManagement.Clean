11caef9fe7df45dd8bba975d35e43b6039f4974a
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-09-25 17:42:52 -0700

    Implement Repository pattern

---
+++ src/Infrastructure/HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HrDatabaseContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("HrSqlConnectionString"));
+                          });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, ILeaveTypeRepository>();
+            services.AddScoped <ILeaveAllocationRepository, ILeaveAllocationRepository>();
+            services.AddScoped <ILeaveRequestRepository, ILeaveRequestRepository>();
+
+            return services;
+        }
+
+    }
+}
---
+++ src/Infrastructure/HR.LeaveManagement.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain.Common;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        // Changed from private to make accessible to derived repositories
+        protected readonly HrDatabaseContext _context;
+
+        public GenericRepository(HrDatabaseContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync()
+        {
+            // Because we don't need to track, and it will help with performance, don't track requests
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            // Couldn't use FindAsync(id) with AsNoTracking()
+            return await _context.Set<T>().AsNoTracking()
+                                          .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            // _context.Update(entity); // This modifies all fields in the entity...
+
+            // Trevoir says this will generate a more efficient statement. TODO: observe the difference
+            _context.Entry(entity).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
