a25624cf78b07ef602405ae2ce2bfa6845a19e2b
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-09-27 22:57:27 -0700

    Unit Test project and initial tests

---
+++ HR.LeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/CreateleaveTypeCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.MappingProfiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace HR.LeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries
+{
+    public class CreateleaveTypeCommandTests
+    {
+        private Mock<ILeaveTypeRepository> _mockRepo;
+        private IMapper _mapper;
+        private Mock<IAppLogger<CreateLeaveTypeCommandHandler>> _mockAppLogger;
+
+        public CreateleaveTypeCommandTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<LeaveTypeProfile>();
+            });
+
+
+            _mapper = mapperConfig.CreateMapper();
+            _mockAppLogger = new Mock<IAppLogger<CreateLeaveTypeCommandHandler>>();
+        }
+
+        [Fact]
+        public async Task Handle_ValidLeaveType()
+        {
+            var handler = new CreateLeaveTypeCommandHandler(_mapper, _mockRepo.Object, _mockAppLogger.Object);
+
+            await handler.Handle(new CreateLeaveTypeCommand()
+            {
+                Name = "Test1",
+                DefaultDays = 1
+            }, CancellationToken.None);
+
+            var leaveTypes = await _mockRepo.Object.GetAsync();
+            leaveTypes.Count.ShouldBe(MockLeaveTypeRepository.MockLeaveTypeRows + 1);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,43 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public class MockLeaveTypeRepository
+    {
+        public static int MockLeaveTypeRows {get; set;}
+
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType { Id = 1, Name = "Test AdHoc", DefaultDays=2 },
+                new LeaveType { Id = 1, Name = "Test Sick", DefaultDays=6 },
+                new LeaveType { Id = 1, Name = "Test Vacation", DefaultDays=15 },
+                new LeaveType { Id = 1, Name = "Test LOA", DefaultDays=180 },
+                new LeaveType { Id = 1, Name = "Bogus", DefaultDays=1 }
+            };
+
+            MockLeaveTypeRows = leaveTypes.Count;
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+            mockRepo.Setup(r => r.IsLeaveTypeUnique(It.IsAny<string>()))
+                .ReturnsAsync((string name) => {
+                    return !leaveTypes.Any(q => q.Name == name);
+                });
+
+            return mockRepo;
+        }
+    }
+}
